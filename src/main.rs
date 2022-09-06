78e830946650b1782b4a5575b360c118f24ede66
samuelsleight <samuel@sleig.ht>
2022-09-06 20:35:12 +0100

    Infinite map scrolling 🎉

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use enumset::{EnumSet, EnumSetType};
 use renderer::{InitError, Renderer};
 use stateloop::{
     app::{App, Data, Event, Window},
     state::Action,
     states,
+    winit::event::{ElementState, VirtualKeyCode},
 };
 use std::{
     collections::{
@@ -22,9 +24,17 @@ use world::{ChunkKey, World};
 mod renderer;
 mod world;
 
+#[derive(Debug, EnumSetType)]
+pub enum InputState {
+    Up,
+    Down,
+    Right,
+    Left,
+}
+
 states! {
     State {
-        MainHandler Main()
+        MainHandler Main(input: EnumSet<InputState>)
     }
 }
 
@@ -37,6 +47,8 @@ struct Storage {
     renderer: Renderer,
     world: World,
     bounds: (u32, u32),
+    offset: (i64, i64),
+    chunk_offset: (i64, i64),
     textures: HashMap<ChunkKey, TextureEntry>,
 }
 
@@ -48,7 +60,9 @@ impl Storage {
             renderer,
             world: World::new(),
             bounds: (0, 0),
-            textures: HashMap::new()
+            offset: (0, 0),
+            chunk_offset: (0, 0),
+            textures: HashMap::new(),
         };
 
         storage.update_bounds(surface);
@@ -56,26 +70,77 @@ impl Storage {
     }
 
     fn update_bounds(&mut self, surface: &Arc<Surface<Window>>) {
-        let window_bounds = surface.window().inner_size().to_logical::<u32>(surface.window().scale_factor());
+        let window_bounds = surface
+            .window()
+            .inner_size()
+            .to_logical::<u32>(surface.window().scale_factor());
         let chunk_size = 300u32;
-        self.bounds = (window_bounds.width / chunk_size, window_bounds.height / chunk_size);
+        self.bounds = (
+            window_bounds.width / chunk_size,
+            window_bounds.height / chunk_size,
+        );
     }
 }
 
 impl MainHandler for AppData {
-    fn handle_event(&mut self, event: Event) -> Action<State> {
+    fn handle_event(
+        &mut self,
+        event: Event,
+        mut input_state: EnumSet<InputState>,
+    ) -> Action<State> {
         match event {
+            Event::KeyboardInput { ref input, .. } => {
+                let input_kind = match input.virtual_keycode {
+                    Some(VirtualKeyCode::Up | VirtualKeyCode::W) => InputState::Up,
+                    Some(VirtualKeyCode::Down | VirtualKeyCode::S) => InputState::Down,
+                    Some(VirtualKeyCode::Right | VirtualKeyCode::D) => InputState::Right,
+                    Some(VirtualKeyCode::Left | VirtualKeyCode::A) => InputState::Left,
+                    _ => return Action::Continue,
+                };
+
+                if input.state == ElementState::Pressed {
+                    input_state.insert(input_kind);
+                } else {
+                    input_state.remove(input_kind);
+                }
+
+                Action::Done(State::Main(input_state))
+            }
             Event::Resized(..) => {
                 let window = self.window().clone();
                 self.data.update_bounds(&window);
                 Action::Continue
-            },
+            }
+
             Event::CloseRequested => Action::Quit,
             _ => Action::Continue,
         }
     }
 
-    fn handle_tick(&mut self) {
+    fn handle_tick(&mut self, input_state: EnumSet<InputState>) {
+        let speed = 5;
+
+        if input_state.contains(InputState::Up) {
+            self.data.offset.1 += speed;
+        }
+
+        if input_state.contains(InputState::Down) {
+            self.data.offset.1 -= speed;
+        }
+
+        if input_state.contains(InputState::Right) {
+            self.data.offset.0 -= speed;
+        }
+
+        if input_state.contains(InputState::Left) {
+            self.data.offset.0 += speed;
+        }
+
+        self.data.chunk_offset = (
+            (self.data.offset.0 + 150) / 300 as i64,
+            (self.data.offset.1 + 150) / 300 as i64,
+        );
+
         while let Some(chunk) = self.data.world.get_chunk_result() {
             self.data.textures.insert(
                 chunk.key,
@@ -88,9 +153,12 @@ impl MainHandler for AppData {
             );
         }
 
-        for x in 0..=self.data.bounds.0 {
-            for y in 0..=self.data.bounds.1 {
-                let key = ChunkKey::new(x as i64, y as i64);
+        for x in (0..=self.data.bounds.0 + 2).map(|x| x as i64 - 1) {
+            for y in (0..=self.data.bounds.1 + 2).map(|y| y as i64 - 1) {
+                let key = ChunkKey::new(
+                    x as i64 - self.data.chunk_offset.0,
+                    y as i64 - self.data.chunk_offset.1,
+                );
 
                 match self.data.textures.entry(key) {
                     Occupied(_) => continue,
@@ -103,14 +171,21 @@ impl MainHandler for AppData {
         }
     }
 
-    fn handle_render(&self) {
+    fn handle_render(&self, _: EnumSet<InputState>) {
         self.data.renderer.render(self.window(), |mut frame| {
-            for x in 0..=self.data.bounds.0 {
-                for y in 0..=self.data.bounds.1 {
-                let key = ChunkKey::new(x as i64, y as i64);
+            for x in (0..=self.data.bounds.0 + 2).map(|x| x as i64 - 1) {
+                for y in (0..=self.data.bounds.1 + 2).map(|y| y as i64 - 1) {
+                    let key = ChunkKey::new(
+                        x as i64 - self.data.chunk_offset.0,
+                        y as i64 - self.data.chunk_offset.1,
+                    );
                     if let Some(&TextureEntry::Valid(ref texture)) = self.data.textures.get(&key) {
                         frame = frame.draw(
-                            [(key.x * 300) as f32, (key.y * 300) as f32].into(),
+                            [
+                                (self.data.offset.0 + key.x * 300) as f32,
+                                (self.data.offset.1 + key.y * 300) as f32,
+                            ]
+                            .into(),
                             texture.clone(),
                         );
                     }
@@ -144,5 +219,5 @@ fn main() {
         },
     )
     .expect("Unable to initialise application")
-    .run(60, State::Main())
+    .run(60, State::Main(EnumSet::empty()))
 }
